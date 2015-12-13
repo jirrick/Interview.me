f4efc22bbe5b6cb7f2ac24643ac1ece928aec9d8
Jan Cislinsky <jan.cislinsky@letsgood.com>
2015-12-13 21:40:18 +0100

    Edit form with ability to create question

--- application/models/Question.php
+++ application/models/Question.php
@@ -10,10 +10,19 @@ class Question extends My_Db_Table_Row {
      * Vrati rowset s moznostmi k otazce
      *
      */
-     public function getOptions() {
+    public function getOptions()
+    {
         return $this->findDependentRowset('Options');
     }
-	    
-   
+
+    /**
+     * Vrací true pokud existují odpovědi k této otázce.
+     */
+    public function isAnswered()
+    {
+        $r = My_Model::get('Responses');
+        $responses = $r->fetchAll($r->select()->where('id_otazka = ?', $this->getid_otazka()));
+        return $responses->count() > 0;
+    }
 }
 	
